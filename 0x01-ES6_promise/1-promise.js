db6aea86d2c5b331683312573719c44cc004f921
Baridoo Saro <sarobaridoo@gmail.com>
2024-08-07 21:53:33 +0000

    fixed

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
-export default function (boolean) {
-	boolean = true;
+export default function getFullResponseFromAPI(success) {
 	return new Promise ((resolve, reject) => {
 		const object = {
 			status: 200,
 			body: 'Success',
 		};
-		if (boolean) {
+		if (success === true) {
 			resolve(object);
 		} else{
 			reject(Error('The fake API is not working currently'));
